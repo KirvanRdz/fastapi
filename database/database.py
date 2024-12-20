2451c07ebe55ccbd2f22850503c016f9b76384b8
KirvanRdz <kirvanrodriguez@hotmail.com>
2024-12-20 13:27:19 -0600

    Configurar Docker

--- database/database.py
+++ database/database.py
@@ -3,11 +3,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-sqliteName= 'movies.sqlite'
-base_dir=os.path.dirname(os.path.realpath(__file__))
-databaseUrl= f'sqlite:///{os.path.join(base_dir,sqliteName)}'
+# Nombre del archivo SQLite
+sqlite_name = os.getenv("SQLITE_NAME", "movies.sqlite")
+# Directorio base para SQLite
+base_dir = os.getenv("SQLITE_DIR", os.path.dirname(os.path.realpath(__file__)))
+# URL de la base de datos
+database_url = f"sqlite:///{os.path.join(base_dir, sqlite_name)}"
 
-engine= create_engine(databaseUrl,echo=True)
-session= sessionmaker(bind=engine)
+# Configurar SQLAlchemy
+engine = create_engine(database_url, echo=True)
+session = sessionmaker(bind=engine)
 
-Base= declarative_base()
+Base = declarative_base()
